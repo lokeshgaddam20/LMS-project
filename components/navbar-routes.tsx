198c023e8a93fd6542be3e899f0bd43be58ea410
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-09-25 23:23:32 +0530

    started navbar-routes

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import  { usePathname, useRouter } from "next/navigation";
 
 export const NavbarRoutes = () => {
+    const pathname = usePathname();
+    const router = useRouter();
+
     return(
         <div className="flex gap-x-2 ml-auto">
             <UserButton/>
