d33f89242223bda98b41568f190e7b889eb343cf
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-09-25 23:29:05 +0530

    progress

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -2,16 +2,23 @@
 
 import { UserButton } from "@clerk/nextjs";
 import  { usePathname, useRouter } from "next/navigation";
+import {Button} from "@/components/ui/button";
+import  { LogOut } from "lucide-react";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
     const router = useRouter();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
-    const isPlayerPage = pathname?.startsWith("/player");
+    const isPlayerPage = pathname?.startsWith("/chapter");
 
     return(
         <div className="flex gap-x-2 ml-auto">
+            {isTeacherPage || isPlayerPage ? (
+                <Button>
+                    <LogOut />
+                </Button>
+            ): null}
             <UserButton/>
         </div>
     )
