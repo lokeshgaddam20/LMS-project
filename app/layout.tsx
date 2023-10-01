26fdcdf678c0b345f861f2f359a60856fc8a3a73
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-10-01 19:29:26 +0530

    Toastprovider add

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import { ToastProvider } from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
